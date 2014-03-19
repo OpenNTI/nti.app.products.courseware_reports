3556e5580d7c16f5e52eab5d6d479bf30433b55e
Josh Zuech <josh.zuech@nextthought.com>
2014-03-19 23:23:20 +0000

    Expose links for other reports
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware_reports/trunk@35179 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/coursewarereports/decorators.py
+++ src/nti/app/products/coursewarereports/decorators.py
@@ -16,10 +16,21 @@ from pyramid.interfaces import IRequest
 from nti.externalization import interfaces as ext_interfaces
 from nti.app.products.courseware.interfaces import ICourseInstanceEnrollment
 
+from nti.dataserver.contenttypes.forums.interfaces import ICommunityForum
+from nti.dataserver.contenttypes.forums.interfaces import ICommunityHeadlineTopic
+
+from nti.contenttypes.courses.interfaces import ICourseInstance
+
+from nti.app.products.gradebook.interfaces import IGradeBookEntry
+
 from nti.app.renderers.decorators import AbstractAuthenticatedRequestAwareDecorator
 
 from . import MessageFactory as _
 from . import VIEW_STUDENT_PARTICIPATION
+from . import VIEW_FORUM_PARTICIPATION
+from . import VIEW_TOPIC_PARTICIPATION
+from . import VIEW_COURSE_SUMMARY
+from . import VIEW_ASSIGNMENT_SUMMARY
 
 LINKS = ext_interfaces.StandardExternalFields.LINKS
 from nti.dataserver.links import Link
@@ -37,3 +48,54 @@ class _StudentParticipationReport(AbstractAuthenticatedRequestAwareDecorator):
 							elements=(VIEW_STUDENT_PARTICIPATION,),
 							title=_('Student Participation Report')) )	
 		
+@interface.implementer(ext_interfaces.IExternalMappingDecorator)
+@component.adapter(ICommunityForum, IRequest)
+class _ForumParticipationReport(AbstractAuthenticatedRequestAwareDecorator):
+	"""
+	A link to return the forum participation report.
+	"""
+	def _do_decorate_external( self, context, result_map ):
+		links = result_map.setdefault( LINKS, [] )
+		links.append( Link( context,
+							rel='report-%s' % VIEW_FORUM_PARTICIPATION,
+							elements=(VIEW_FORUM_PARTICIPATION,),
+							title=_('Forum Participation Report')) )
+		
+@interface.implementer(ext_interfaces.IExternalMappingDecorator)
+@component.adapter(ICommunityHeadlineTopic, IRequest)
+class _TopicParticipationReport(AbstractAuthenticatedRequestAwareDecorator):
+	"""
+	A link to return the topic participation report.
+	"""
+	def _do_decorate_external( self, context, result_map ):
+		links = result_map.setdefault( LINKS, [] )
+		links.append( Link( context,
+							rel='report-%s' % VIEW_TOPIC_PARTICIPATION,
+							elements=(VIEW_TOPIC_PARTICIPATION,),
+							title=_('Topic Participation Report')) )	
+		
+@interface.implementer(ext_interfaces.IExternalMappingDecorator)
+@component.adapter(ICourseInstance, IRequest)
+class _CourseSummaryReport(AbstractAuthenticatedRequestAwareDecorator):
+	"""
+	A link to return the course summary report.
+	"""
+	def _do_decorate_external( self, context, result_map ):
+		links = result_map.setdefault( LINKS, [] )
+		links.append( Link( context,
+							rel='report-%s' % VIEW_COURSE_SUMMARY,
+							elements=(VIEW_COURSE_SUMMARY,),
+							title=_('Course Summary Report')) )		
+		
+@interface.implementer(ext_interfaces.IExternalMappingDecorator)
+@component.adapter(IGradeBookEntry, IRequest)
+class _AssignmentSummaryReport(AbstractAuthenticatedRequestAwareDecorator):
+	"""
+	A link to return the assignment summary report.
+	"""
+	def _do_decorate_external( self, context, result_map ):
+		links = result_map.setdefault( LINKS, [] )
+		links.append( Link( context,
+							rel='report-%s' % VIEW_ASSIGNMENT_SUMMARY,
+							elements=(VIEW_ASSIGNMENT_SUMMARY,),
+							title=_('Assignment Summary Report')) )									
--- src/nti/app/products/coursewarereports/tests/test_decorators.py
+++ src/nti/app/products/coursewarereports/tests/test_decorators.py
@@ -1,4 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
 
+
+.. $Id$
+"""
 
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
@@ -6,21 +12,31 @@ __docformat__ = "restructuredtext en"
 logger = __import__('logging').getLogger(__name__)
 
 from nti.testing import base
-from nti.app.products.coursewarereports.decorators import _StudentParticipationReport
+
 from nti.externalization import interfaces as ext_interfaces
 
 import unittest
 
 from hamcrest import *
 
+from nti.app.products.coursewarereports.decorators import _StudentParticipationReport
+from nti.app.products.coursewarereports.decorators import _ForumParticipationReport
+from nti.app.products.coursewarereports.decorators import _TopicParticipationReport
+from nti.app.products.coursewarereports.decorators import _CourseSummaryReport
+from nti.app.products.coursewarereports.decorators import _AssignmentSummaryReport
+
 from .. import VIEW_STUDENT_PARTICIPATION
+from .. import VIEW_FORUM_PARTICIPATION
+from .. import VIEW_TOPIC_PARTICIPATION
+from .. import VIEW_COURSE_SUMMARY
+from .. import VIEW_ASSIGNMENT_SUMMARY
 
 LINKS = ext_interfaces.StandardExternalFields.LINKS
 from nti.dataserver.links import Link
 
 class TestDecorators(unittest.TestCase):
 
-		def test_decorator( self ):
+		def test_student_participation_decorator( self ):
 			spr = _StudentParticipationReport( object(), None )
 			result = {}
 			spr._do_decorate_external( object(), result )
@@ -30,3 +46,47 @@ class TestDecorators(unittest.TestCase):
 		
 			assert_that( result, has_entry( 'Links',
 									contains( has_property( 'rel', 'report-%s' % VIEW_STUDENT_PARTICIPATION ))))
+			
+		def test_forum_participation_decorator( self ):
+			spr = _ForumParticipationReport( object(), None )
+			result = {}
+			spr._do_decorate_external( object(), result )
+		
+			assert( result is not None )
+			assert_that( result, is_( not_none() ) )
+		
+			assert_that( result, has_entry( 'Links',
+									contains( has_property( 'rel', 'report-%s' % VIEW_FORUM_PARTICIPATION ))))
+			
+		def test_topic_participation_decorator( self ):
+			spr = _TopicParticipationReport( object(), None )
+			result = {}
+			spr._do_decorate_external( object(), result )
+		
+			assert( result is not None )
+			assert_that( result, is_( not_none() ) )
+		
+			assert_that( result, has_entry( 'Links',
+									contains( has_property( 'rel', 'report-%s' % VIEW_TOPIC_PARTICIPATION ))))
+			
+		def test_course_summary_decorator( self ):
+			spr = _CourseSummaryReport( object(), None )
+			result = {}
+			spr._do_decorate_external( object(), result )
+		
+			assert( result is not None )
+			assert_that( result, is_( not_none() ) )
+		
+			assert_that( result, has_entry( 'Links',
+									contains( has_property( 'rel', 'report-%s' % VIEW_COURSE_SUMMARY ))))
+			
+		def test_assignment_history_decorator( self ):
+			spr = _AssignmentSummaryReport( object(), None )
+			result = {}
+			spr._do_decorate_external( object(), result )
+		
+			assert( result is not None )
+			assert_that( result, is_( not_none() ) )
+		
+			assert_that( result, has_entry( 'Links',
+									contains( has_property( 'rel', 'report-%s' % VIEW_ASSIGNMENT_SUMMARY ))))												
