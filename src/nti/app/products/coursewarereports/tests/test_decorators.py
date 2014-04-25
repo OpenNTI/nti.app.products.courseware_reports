52362bec334fc716f31a934f063f9ac31eb38770
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-04-25 00:02:32 +0000

    clean up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware_reports/trunk@37901 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/coursewarereports/tests/test_decorators.py
+++ src/nti/app/products/coursewarereports/tests/test_decorators.py
@@ -1,92 +1,86 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-
-
-.. $Id$
-"""
 
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
-logger = __import__('logging').getLogger(__name__)
-
-from nti.testing import base
-
-from nti.externalization import interfaces as ext_interfaces
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
 
 import unittest
 
-from hamcrest import *
+from hamcrest import is_
+from hamcrest import contains
+from hamcrest import not_none
+from hamcrest import has_entry
+from hamcrest import assert_that
+from hamcrest import has_property
 
-from nti.app.products.coursewarereports.decorators import _StudentParticipationReport
-from nti.app.products.coursewarereports.decorators import _ForumParticipationReport
-from nti.app.products.coursewarereports.decorators import _TopicParticipationReport
 from nti.app.products.coursewarereports.decorators import _CourseSummaryReport
 from nti.app.products.coursewarereports.decorators import _AssignmentSummaryReport
+from nti.app.products.coursewarereports.decorators import _ForumParticipationReport
+from nti.app.products.coursewarereports.decorators import _TopicParticipationReport
+from nti.app.products.coursewarereports.decorators import _StudentParticipationReport
 
-from .. import VIEW_STUDENT_PARTICIPATION
-from .. import VIEW_FORUM_PARTICIPATION
-from .. import VIEW_TOPIC_PARTICIPATION
 from .. import VIEW_COURSE_SUMMARY
 from .. import VIEW_ASSIGNMENT_SUMMARY
-
-LINKS = ext_interfaces.StandardExternalFields.LINKS
-from nti.dataserver.links import Link
+from .. import VIEW_FORUM_PARTICIPATION
+from .. import VIEW_TOPIC_PARTICIPATION
+from .. import VIEW_STUDENT_PARTICIPATION
 
 class TestDecorators(unittest.TestCase):
 
-		def test_student_participation_decorator( self ):
-			spr = _StudentParticipationReport( object(), None )
-			result = {}
-			spr._do_decorate_external( object(), result )
+	def test_student_participation_decorator(self):
+		spr = _StudentParticipationReport(object(), None)
+		result = {}
+		spr._do_decorate_external(object(), result)
 
-			assert( result is not None )
-			assert_that( result, is_( not_none() ) )
+		assert(result is not None)
+		assert_that(result, is_(not_none()))
 
-			assert_that( result, has_entry( 'Links',
-									contains( has_property( 'rel', 'report-%s' % VIEW_STUDENT_PARTICIPATION ))))
+		assert_that(result, has_entry('Links',
+								contains(has_property('rel', 'report-%s' % VIEW_STUDENT_PARTICIPATION))))
 
-		def test_forum_participation_decorator( self ):
-			spr = _ForumParticipationReport( object(), None )
-			result = {}
-			spr._do_decorate_external( object(), result )
+	def test_forum_participation_decorator(self):
+		spr = _ForumParticipationReport(object(), None)
+		result = {}
+		spr._do_decorate_external(object(), result)
 
-			assert( result is not None )
-			assert_that( result, is_( not_none() ) )
+		assert(result is not None)
+		assert_that(result, is_(not_none()))
 
-			assert_that( result, has_entry( 'Links',
-									contains( has_property( 'rel', 'report-%s' % VIEW_FORUM_PARTICIPATION ))))
+		assert_that(result, has_entry('Links',
+								contains(has_property('rel', 'report-%s' % VIEW_FORUM_PARTICIPATION))))
 
-		def test_topic_participation_decorator( self ):
-			spr = _TopicParticipationReport( object(), None )
-			result = {}
-			spr._do_decorate_external( object(), result )
+	def test_topic_participation_decorator(self):
+		spr = _TopicParticipationReport(object(), None)
+		result = {}
+		spr._do_decorate_external(object(), result)
 
-			assert( result is not None )
-			assert_that( result, is_( not_none() ) )
+		assert(result is not None)
+		assert_that(result, is_(not_none()))
 
-			assert_that( result, has_entry( 'Links',
-									contains( has_property( 'rel', 'report-%s' % VIEW_TOPIC_PARTICIPATION ))))
+		assert_that(result, has_entry('Links',
+								contains(has_property('rel', 'report-%s' % VIEW_TOPIC_PARTICIPATION))))
 
-		def test_course_summary_decorator( self ):
-			spr = _CourseSummaryReport( object(), None )
-			result = {}
-			spr._do_decorate_external( object(), result )
+	def test_course_summary_decorator(self):
+		spr = _CourseSummaryReport(object(), None)
+		result = {}
+		spr._do_decorate_external(object(), result)
 
-			assert( result is not None )
-			assert_that( result, is_( not_none() ) )
+		assert(result is not None)
+		assert_that(result, is_(not_none()))
 
-			assert_that( result, has_entry( 'Links',
-									contains( has_property( 'rel', 'report-%s' % VIEW_COURSE_SUMMARY ))))
+		assert_that(result, has_entry('Links',
+								contains(has_property('rel', 'report-%s' % VIEW_COURSE_SUMMARY))))
 
-		def test_assignment_history_decorator( self ):
-			spr = _AssignmentSummaryReport( object(), None )
-			result = {}
-			spr._do_decorate_external( object(), result )
+	def test_assignment_history_decorator(self):
+		spr = _AssignmentSummaryReport(object(), None)
+		result = {}
+		spr._do_decorate_external(object(), result)
 
-			assert( result is not None )
-			assert_that( result, is_( not_none() ) )
+		assert(result is not None)
+		assert_that(result, is_(not_none()))
 
-			assert_that( result, has_entry( 'Links',
-									contains( has_property( 'rel', 'report-%s' % VIEW_ASSIGNMENT_SUMMARY ))))
+		assert_that(result, has_entry('Links',
+								contains(has_property('rel', 'report-%s' % VIEW_ASSIGNMENT_SUMMARY))))
