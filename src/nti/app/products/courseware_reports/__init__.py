500460ecbbf5067270ac70cbab71e528f49f9240
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-09-08 19:23:00 +0000

    make space
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware_reports/trunk@72567 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware_reports/__init__.py
+++ src/nti/app/products/courseware_reports/__init__.py
@@ -9,12 +9,13 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+VIEW_SURVEY_REPORT = 'SurveyReport.pdf'
+VIEW_VIDEO_REPORT = "VideoUsageReport.pdf"
 VIEW_COURSE_SUMMARY = "CourseSummaryReport.pdf"
 VIEW_ASSIGNMENT_SUMMARY = 'AssignmentSummaryReport.pdf'
 VIEW_FORUM_PARTICIPATION = "ForumParticipationReport.pdf"
 VIEW_TOPIC_PARTICIPATION = "TopicParticipationReport.pdf"
 VIEW_STUDENT_PARTICIPATION = "StudentParticipationReport.pdf"
-VIEW_VIDEO_REPORT = "VideoUsageReport.pdf"
 
 import zope.i18nmessageid
 MessageFactory = zope.i18nmessageid.MessageFactory('nti.app.products.courseware_reports')
---
+++ src/nti/app/products/courseware_reports/views/survey_views.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from .. import MessageFactory as _
+
+from six import string_types
+
+from zope import component
+
+from pyramid.view import view_config
+
+from nti.app.assessment.common import aggregate_course_inquiry
+
+from nti.assessment.interfaces import IQSurvey
+
+from nti.contentfragments.interfaces import IPlainTextContentFragment
+
+from .. import VIEW_SURVEY_REPORT
+
+from .view_mixins import _AbstractReportView
+
+@view_config(context=IQSurvey,
+			 name=VIEW_SURVEY_REPORT)
+class SurveyReportPdf(_AbstractReportView):
+
+	report_title = _('Survey Report')
+
+	def _build_question_data(self, options):
+		survey = component.queryUtility(IQSurvey, name=self.context.inquryId)
+		if survey is None:
+			options['question_stats'] = None
+			return
+
+		# TODO Need to handle randomized questions.
+		# - We might get this for free since we store our questions by ntiids.
+		# - Verify.
+		course = None
+		aggregate_course_inquiry(survey, course)
+
+		# options['question_stats'] = _build_question_stats(ordered_questions, question_stats)
+
+	def _get_displayable(self, source):
+		if isinstance(source, string_types):
+			source = IPlainTextContentFragment(source)
+		return source
+
+	def __call__(self):
+		self._check_access()
+		options = self.options
+		self._build_question_data(options)
+		return options
