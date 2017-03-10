679cca5c7fe61404b2915a868fd84b0a853c9366
Zachary Roux <zachary.roux@nextthought.com>
2017-03-10 22:56:53 +0000

    Add test for video completion function
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware_reports/trunk@108610 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware_reports/tests/test_views.py
+++ src/nti/app/products/courseware_reports/tests/test_views.py
@@ -8,14 +8,20 @@ __docformat__ = "restructuredtext en"
 # pylint: disable=W0212,R0904
 
 from hamcrest import assert_that
+from hamcrest import is_
 from hamcrest import has_property
 from hamcrest import contains_string
+
+import unittest
+import fudge
 
 from nti.app.products.courseware_reports import VIEW_COURSE_SUMMARY
 from nti.app.products.courseware_reports import VIEW_ASSIGNMENT_SUMMARY
 from nti.app.products.courseware_reports import VIEW_FORUM_PARTICIPATION
 from nti.app.products.courseware_reports import VIEW_TOPIC_PARTICIPATION
 from nti.app.products.courseware_reports import VIEW_STUDENT_PARTICIPATION
+
+from nti.app.products.courseware_reports.utils import _check_video_completion
 
 from nti.app.assessment.tests import RegisterAssignmentLayerMixin
 from nti.app.assessment.tests import RegisterAssignmentsForEveryoneLayer
@@ -25,150 +31,187 @@ from nti.app.products.courseware.tests import InstructedCourseApplicationTestLay
 from nti.app.testing.decorators import WithSharedApplicationMockDS
 from nti.app.testing.application_webtest import ApplicationLayerTest
 
+
 class TestStudentParticipationReport(ApplicationLayerTest):
 
-	layer = RegisterAssignmentsForEveryoneLayer
+    layer = RegisterAssignmentsForEveryoneLayer
 
-	default_origin = b'http://janux.ou.edu'
+    default_origin = b'http://janux.ou.edu'
 
-	@WithSharedApplicationMockDS(users=True,testapp=True,default_authenticate=True)
-	def test_application_view_empty_report(self):
-		# Trivial test to make sure we can fetch the report even with
-		# no data.
-		self.testapp.post_json( '/dataserver2/users/sjohnson@nextthought.com/Courses/EnrolledCourses',
-								'tag:nextthought.com,2011-10:NTI-CourseInfo-Fall2013_CLC3403_LawAndJustice',
-								status=201 )
+    @WithSharedApplicationMockDS(users=True, testapp=True, default_authenticate=True)
+    def test_application_view_empty_report(self):
+        # Trivial test to make sure we can fetch the report even with
+        # no data.
+        self.testapp.post_json('/dataserver2/users/sjohnson@nextthought.com/Courses/EnrolledCourses',
+                               'tag:nextthought.com,2011-10:NTI-CourseInfo-Fall2013_CLC3403_LawAndJustice',
+                               status=201)
 
-		instructor_environ = self._make_extra_environ(username='harp4162')
-		admin_courses = self.testapp.get( '/dataserver2/users/harp4162/Courses/AdministeredCourses/',
-										extra_environ=instructor_environ)
+        instructor_environ = self._make_extra_environ(username='harp4162')
+        admin_courses = self.testapp.get('/dataserver2/users/harp4162/Courses/AdministeredCourses/',
+                                         extra_environ=instructor_environ)
 
-		# Get our student from the roster
-		course_instance = admin_courses.json_body.get( 'Items' )[0].get( 'CourseInstance' )
-		roster_link = self.require_link_href_with_rel( course_instance, 'CourseEnrollmentRoster')
-		sj_enrollment = self.testapp.get( roster_link,
-										extra_environ=instructor_environ)
-		sj_enrollment = sj_enrollment.json_body.get( 'Items' )[0]
+        # Get our student from the roster
+        course_instance = admin_courses.json_body.get(
+            'Items')[0].get('CourseInstance')
+        roster_link = self.require_link_href_with_rel(
+            course_instance, 'CourseEnrollmentRoster')
+        sj_enrollment = self.testapp.get(roster_link,
+                                         extra_environ=instructor_environ)
+        sj_enrollment = sj_enrollment.json_body.get('Items')[0]
 
-		view_href = self.require_link_href_with_rel( sj_enrollment,
-													'report-%s' % VIEW_STUDENT_PARTICIPATION )
+        view_href = self.require_link_href_with_rel(sj_enrollment,
+                                                    'report-%s' % VIEW_STUDENT_PARTICIPATION)
 
-		res = self.testapp.get(view_href, extra_environ=instructor_environ)
-		assert_that( res, has_property('content_type', 'application/pdf'))
+        res = self.testapp.get(view_href, extra_environ=instructor_environ)
+        assert_that(res, has_property('content_type', 'application/pdf'))
+
 
 class TestForumParticipationReport(ApplicationLayerTest):
 
-	layer = InstructedCourseApplicationTestLayer
+    layer = InstructedCourseApplicationTestLayer
 
-	default_origin = b'http://janux.ou.edu'
+    default_origin = b'http://janux.ou.edu'
 
-	@WithSharedApplicationMockDS(users=True,testapp=True,default_authenticate=True)
-	def test_link(self):
-		enrollment_res = self.testapp.post_json( '/dataserver2/users/sjohnson@nextthought.com/Courses/EnrolledCourses',
-								'tag:nextthought.com,2011-10:NTI-CourseInfo-Fall2013_CLC3403_LawAndJustice',
-								status=201 )
+    @WithSharedApplicationMockDS(users=True, testapp=True, default_authenticate=True)
+    def test_link(self):
+        enrollment_res = self.testapp.post_json('/dataserver2/users/sjohnson@nextthought.com/Courses/EnrolledCourses',
+                                                'tag:nextthought.com,2011-10:NTI-CourseInfo-Fall2013_CLC3403_LawAndJustice',
+                                                status=201)
 
-		board_href = enrollment_res.json_body['CourseInstance']['Discussions']['href']
-		forum_href = board_href + '/Forum'
-		instructor_environ = self._make_extra_environ(username='harp4162')
+        board_href = enrollment_res.json_body[
+            'CourseInstance']['Discussions']['href']
+        forum_href = board_href + '/Forum'
+        instructor_environ = self._make_extra_environ(username='harp4162')
 
-		forum_res = self.testapp.get( forum_href, extra_environ=instructor_environ )
+        forum_res = self.testapp.get(
+            forum_href, extra_environ=instructor_environ)
 
-		self.forbid_link_with_rel( forum_res.json_body, 'report-' + VIEW_FORUM_PARTICIPATION )
+        self.forbid_link_with_rel(
+            forum_res.json_body, 'report-' + VIEW_FORUM_PARTICIPATION)
+
 
 class TestTopicParticipationReport(ApplicationLayerTest):
 
-	layer = InstructedCourseApplicationTestLayer
+    layer = InstructedCourseApplicationTestLayer
 
-	default_origin = b'http://janux.ou.edu'
+    default_origin = b'http://janux.ou.edu'
 
-	@WithSharedApplicationMockDS(users=True,testapp=True,default_authenticate=True)
-	def test_link(self):
-		enrollment_res = self.testapp.post_json( '/dataserver2/users/sjohnson@nextthought.com/Courses/EnrolledCourses',
-								'tag:nextthought.com,2011-10:NTI-CourseInfo-Fall2013_CLC3403_LawAndJustice',
-								status=201 )
+    @WithSharedApplicationMockDS(users=True, testapp=True, default_authenticate=True)
+    def test_link(self):
+        enrollment_res = self.testapp.post_json('/dataserver2/users/sjohnson@nextthought.com/Courses/EnrolledCourses',
+                                                'tag:nextthought.com,2011-10:NTI-CourseInfo-Fall2013_CLC3403_LawAndJustice',
+                                                status=201)
 
-		board_href = enrollment_res.json_body['CourseInstance']['Discussions']['href']
-		forum_href = board_href + '/Forum'
-		instructor_environ = self._make_extra_environ(username='harp4162')
+        board_href = enrollment_res.json_body[
+            'CourseInstance']['Discussions']['href']
+        forum_href = board_href + '/Forum'
+        instructor_environ = self._make_extra_environ(username='harp4162')
 
-		# Create a topic
-		res = self.testapp.post_json( forum_href,
-									  {'Class': 'Post', 'body': ['My body'], 'title': 'my title'},
-									  extra_environ=instructor_environ)
-		self.forbid_link_with_rel( res.json_body, 'report-' + VIEW_TOPIC_PARTICIPATION )
+        # Create a topic
+        res = self.testapp.post_json(forum_href,
+                                     {'Class': 'Post', 'body': [
+                                         'My body'], 'title': 'my title'},
+                                     extra_environ=instructor_environ)
+        self.forbid_link_with_rel(
+            res.json_body, 'report-' + VIEW_TOPIC_PARTICIPATION)
+
 
 class TestCourseSummaryReport(ApplicationLayerTest):
 
-	layer = InstructedCourseApplicationTestLayer
+    layer = InstructedCourseApplicationTestLayer
 
-	default_origin = b'http://janux.ou.edu'
+    default_origin = b'http://janux.ou.edu'
 
-	@WithSharedApplicationMockDS(users=True,testapp=True,default_authenticate=True)
-	def test_application_view_empty_report(self):
-		# Trivial test to make sure we can fetch the report even with
-		# no data.
-		instructor_environ = self._make_extra_environ(username='harp4162')
-		admin_courses = self.testapp.get( '/dataserver2/users/harp4162/Courses/AdministeredCourses/',
-										extra_environ=instructor_environ)
+    @WithSharedApplicationMockDS(users=True, testapp=True, default_authenticate=True)
+    def test_application_view_empty_report(self):
+        # Trivial test to make sure we can fetch the report even with
+        # no data.
+        instructor_environ = self._make_extra_environ(username='harp4162')
+        admin_courses = self.testapp.get('/dataserver2/users/harp4162/Courses/AdministeredCourses/',
+                                         extra_environ=instructor_environ)
 
-		course = admin_courses.json_body.get( 'Items' )[0].get( 'CourseInstance' )
-		report_href = self.require_link_href_with_rel( course, 'report-' + VIEW_COURSE_SUMMARY )
-		assert_that( report_href, contains_string( 'CLC3403' ) )
+        course = admin_courses.json_body.get('Items')[0].get('CourseInstance')
+        report_href = self.require_link_href_with_rel(
+            course, 'report-' + VIEW_COURSE_SUMMARY)
+        assert_that(report_href, contains_string('CLC3403'))
 
-		res = self.testapp.get(report_href, extra_environ=instructor_environ)
-		assert_that( res, has_property('content_type', 'application/pdf') )
+        res = self.testapp.get(report_href, extra_environ=instructor_environ)
+        assert_that(res, has_property('content_type', 'application/pdf'))
 
 from nti.assessment.submission import AssignmentSubmission
 from nti.assessment.submission import QuestionSetSubmission
 from nti.externalization.externalization import to_external_object
 
+
 class TestAssignmentSummaryReport(RegisterAssignmentLayerMixin,
-								  ApplicationLayerTest):
+                                  ApplicationLayerTest):
 
-	layer = RegisterAssignmentsForEveryoneLayer
+    layer = RegisterAssignmentsForEveryoneLayer
 
-	default_origin = b'http://janux.ou.edu'
+    default_origin = b'http://janux.ou.edu'
 
-	assignments_path = '/dataserver2/%2B%2Betc%2B%2Bhostsites/platform.ou.edu/%2B%2Betc%2B%2Bsite/Courses/Fall2013/CLC3403_LawAndJustice/AssignmentsByOutlineNode'
+    assignments_path = '/dataserver2/%2B%2Betc%2B%2Bhostsites/platform.ou.edu/%2B%2Betc%2B%2Bsite/Courses/Fall2013/CLC3403_LawAndJustice/AssignmentsByOutlineNode'
 
-	@WithSharedApplicationMockDS(users=True,testapp=True,default_authenticate=True)
-	def test_link(self):
-		instructor_environ = self._make_extra_environ(username='harp4162')
-		res = self.testapp.get( self.assignments_path,
-								extra_environ=instructor_environ)
+    @WithSharedApplicationMockDS(users=True, testapp=True, default_authenticate=True)
+    def test_link(self):
+        instructor_environ = self._make_extra_environ(username='harp4162')
+        res = self.testapp.get(self.assignments_path,
+                               extra_environ=instructor_environ)
 
-		assignment = res.json_body.get( 'Items' )['tag:nextthought.com,2011-10:OU-HTML-CLC3403_LawAndJustice.sec:QUIZ_01.01'][0]
-		self.forbid_link_with_rel( assignment, 'report-' + VIEW_ASSIGNMENT_SUMMARY )
+        assignment = res.json_body.get('Items')[
+            'tag:nextthought.com,2011-10:OU-HTML-CLC3403_LawAndJustice.sec:QUIZ_01.01'][0]
+        self.forbid_link_with_rel(
+            assignment, 'report-' + VIEW_ASSIGNMENT_SUMMARY)
 
-	@WithSharedApplicationMockDS(users=True,testapp=True,default_authenticate=True)
-	def test_application_view_report(self):
-		instructor_environ = self._make_extra_environ(username='harp4162')
-		res = self.testapp.get( self.assignments_path,
-								extra_environ=instructor_environ)
+    @WithSharedApplicationMockDS(users=True, testapp=True, default_authenticate=True)
+    def test_application_view_report(self):
+        instructor_environ = self._make_extra_environ(username='harp4162')
+        res = self.testapp.get(self.assignments_path,
+                               extra_environ=instructor_environ)
 
-		# No link with no submissions
-		assignment = res.json_body.get( 'Items' )['tag:nextthought.com,2011-10:OU-HTML-CLC3403_LawAndJustice.sec:QUIZ_01.01'][0]
-		assignment_href = assignment.get('href')
-		self.forbid_link_with_rel( assignment, 'report-' + VIEW_ASSIGNMENT_SUMMARY )
+        # No link with no submissions
+        assignment = res.json_body.get('Items')[
+            'tag:nextthought.com,2011-10:OU-HTML-CLC3403_LawAndJustice.sec:QUIZ_01.01'][0]
+        assignment_href = assignment.get('href')
+        self.forbid_link_with_rel(
+            assignment, 'report-' + VIEW_ASSIGNMENT_SUMMARY)
 
-		# Sends an assignment through the application by posting to the assignment
-		qs_submission = QuestionSetSubmission(questionSetId=self.question_set_id)
-		submission = AssignmentSubmission(assignmentId=self.assignment_id, parts=(qs_submission,))
+        # Sends an assignment through the application by posting to the
+        # assignment
+        qs_submission = QuestionSetSubmission(
+            questionSetId=self.question_set_id)
+        submission = AssignmentSubmission(
+            assignmentId=self.assignment_id, parts=(qs_submission,))
 
-		ext_obj = to_external_object( submission )
+        ext_obj = to_external_object(submission)
 
-		# Enroll and submit.
-		self.testapp.post_json( '/dataserver2/users/sjohnson@nextthought.com/Courses/EnrolledCourses',
-								'tag:nextthought.com,2011-10:NTI-CourseInfo-Fall2013_CLC3403_LawAndJustice',
-								status=201 )
+        # Enroll and submit.
+        self.testapp.post_json('/dataserver2/users/sjohnson@nextthought.com/Courses/EnrolledCourses',
+                               'tag:nextthought.com,2011-10:NTI-CourseInfo-Fall2013_CLC3403_LawAndJustice',
+                               status=201)
 
-		self.testapp.post_json( assignment_href, ext_obj )
+        self.testapp.post_json(assignment_href, ext_obj)
 
-		# Now we have proper link
-		res = self.testapp.get( self.assignments_path,
-								extra_environ=instructor_environ)
-		assignment = res.json_body.get( 'Items' )['tag:nextthought.com,2011-10:OU-HTML-CLC3403_LawAndJustice.sec:QUIZ_01.01'][0]
-		report_href = self.require_link_href_with_rel( assignment, 'report-' + VIEW_ASSIGNMENT_SUMMARY )
-		res = self.testapp.get(report_href, extra_environ=instructor_environ)
-		assert_that( res, has_property('content_type', 'application/pdf'))
+        # Now we have proper link
+        res = self.testapp.get(self.assignments_path,
+                               extra_environ=instructor_environ)
+        assignment = res.json_body.get('Items')[
+            'tag:nextthought.com,2011-10:OU-HTML-CLC3403_LawAndJustice.sec:QUIZ_01.01'][0]
+        report_href = self.require_link_href_with_rel(
+            assignment, 'report-' + VIEW_ASSIGNMENT_SUMMARY)
+        res = self.testapp.get(report_href, extra_environ=instructor_environ)
+        assert_that(res, has_property('content_type', 'application/pdf'))
+
+
+class TestCompletionReportStats(unittest.TestCase):
+
+    def test_video_completion(self):
+        fake_video = fudge.Fake().has_attr(video_duration='1:40',
+                                           watch_times=fudge.Fake().has_attr(average_total_watch_time='1:00'))
+        completion = _check_video_completion(fake_video)
+        assert_that(completion, is_(False))
+
+        fake_video = fudge.Fake().has_attr(video_duration='1:40',
+                                           watch_times=fudge.Fake().has_attr(average_total_watch_time='1:30'))
+        completion = _check_video_completion(fake_video)
+        assert_that(completion, is_(True))
