bfb4a629d8419565a184ed35716265e0322e7b91
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-06-06 02:51:16 +0000

    clean up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware_reports/trunk@114322 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware_reports/zcml.py
+++ src/nti/app/products/courseware_reports/zcml.py
@@ -16,26 +16,23 @@ from nti.app.products.courseware_reports.reports import InstructorReport
 from nti.contenttypes.reports.zcml import registerReport
 from nti.contenttypes.reports.zcml import IRegisterReport
 
-from nti.schema.field import TextLine
-
 
 class IRegisterInstructorReport(IRegisterReport):
     """
     Interface representing a registration of a new instructor report, defining behavior
     of the various fields
     """
-    permission = TextLine(title=u"The permission level required to access this report",
-                          required=False)
-
 
 def registerInstructorReport(_context, name, description, interface_context,
-                             supported_types, registration_name=None):
+                             supported_types, permission=None, registration_name=None):
     """
     Take the items from ZCML, turn it into a report object and register it as a
     new utility in the current context
     """
     registerReport(_context, name, description,
-                   interface_context, supported_types,
+                   permission=permission,
+                   supported_types=supported_types,
+                   interface_context=interface_context,
                    registration_name=registration_name,
                    report_class=InstructorReport,
                    report_interface=IInstructorReport)
