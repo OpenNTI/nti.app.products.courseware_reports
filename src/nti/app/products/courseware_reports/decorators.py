239a41f982ad71dcedceca89a1af4b33aca5569f
Austin Graham <austin.graham@nextthought.com>
2017-06-13 19:00:50 +0000

    Got rid of import
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware_reports/trunk@114959 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware_reports/decorators.py
+++ src/nti/app/products/courseware_reports/decorators.py
@@ -5,7 +5,6 @@
 """
 
 from __future__ import print_function, unicode_literals, absolute_import, division
-from nti.assessment._latexplastexdomcompare import grade
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
