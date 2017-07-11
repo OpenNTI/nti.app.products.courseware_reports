ea4741561b4b82280dbe267e771f5938161a4011
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-07-11 23:36:50 +0000

    ensure unicode
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware_reports/trunk@117117 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware_reports/contentproviders.py
+++ src/nti/app/products/courseware_reports/contentproviders.py
@@ -6,7 +6,7 @@ RML content providers.
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
@@ -14,10 +14,12 @@ logger = __import__('logging').getLogger(__name__)
 from zope import interface
 
 from zope.contentprovider.interfaces import IContentProvider
+
 from zope.contentprovider.provider import ContentProviderBase
+
 
 @interface.implementer(IContentProvider)
 class DummyContentProvider(ContentProviderBase):
 
-	def render(self, *args, **kwargs):
-		return "<tr><td/></tr>"
+    def render(self, *args, **kwargs):
+        return u"<tr><td/></tr>"
