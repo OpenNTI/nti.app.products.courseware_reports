20d423a881a7f2d3acb761e33d92fea0336460b0
chris.utz <chris.utz@nextthought.com>
2018-02-13 16:41:06 -0600

    add lastAccessed time to enrollments report as well.

--- src/nti/app/products/courseware_reports/tests/test_views.py
+++ src/nti/app/products/courseware_reports/tests/test_views.py
@@ -602,7 +602,8 @@ class TestUserEnrollmentReport(ApplicationLayerTest):
             assert_that(options, has_entry('enrollments',
                                                        has_items(
                                                            has_key('title'),
-                                                           has_key('createdTime'))))
+                                                           has_key('enrollmentTime'),
+                                                           has_key('lastAccessed'))))
             assert_that(options['enrollments'], has_length(1))
 
 class TestCourseRosterReport(ApplicationLayerTest):
