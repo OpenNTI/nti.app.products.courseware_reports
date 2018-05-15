b2b27cb87cb21e7fd3ba29bbfacf6554ac9a90b8
Josh Zuech <josh.zuech@nextthought.com>
2018-05-15 10:15:29 -0500

    Add user transcript reports

---
+++ src/nti/app/products/courseware_reports/predicates.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+from zope import interface
+from zope import component
+
+from nti.contenttypes.reports.interfaces import IReportAvailablePredicate
+
+from nti.dataserver.authorization import is_site_admin
+from nti.dataserver.authorization import is_admin
+
+from nti.dataserver.interfaces import ISiteAdminUtility
+
+from nti.externalization.interfaces import StandardExternalFields
+
+LINKS = StandardExternalFields.LINKS
+
+logger = __import__('logging').getLogger(__name__)
+
+
+@interface.implementer(IReportAvailablePredicate)
+class UseTranscriptPredicate(object):
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def evaluate(self, unused_report, context, user):
+        result = False
+        if is_admin(user) or context == user:
+            result = True
+        elif is_site_admin(user):
+            admin_utility = component.getUtility(ISiteAdminUtility)
+            result = admin_utility.can_administer_user(user, context)
+        return result
