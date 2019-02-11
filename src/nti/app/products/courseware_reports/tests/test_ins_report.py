041cdf20340dfeaeaced5e132a2826e582ebdac7
Josh Zuech <josh.zuech@nextthought.com>
2019-02-11 15:08:58 -0600

    nti.externalization deprecations

--- src/nti/app/products/courseware_reports/tests/test_ins_report.py
+++ src/nti/app/products/courseware_reports/tests/test_ins_report.py
@@ -37,7 +37,7 @@ from nti.dataserver.tests import mock_dataserver
 from nti.dataserver.users.users import User
 
 from nti.externalization.externalization import to_external_object
-from nti.externalization.externalization import StandardExternalFields
+from nti.externalization.interfaces import StandardExternalFields
 
 from nti.ntiids.ntiids import find_object_with_ntiid
 
